99964161947085126a877cd99dcf41049161a395
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-02 04:46:59 -0500

    update

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoList from './todos/TodoList';
+import './App.css';
+
+const App = () => (
+    <div className="App">
+        <TodoList />
+    </div>
+);
+
+export default App;
